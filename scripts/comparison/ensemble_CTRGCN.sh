b079631896f5d0f41e5141b5a49467ee86059008
lycge20923 <lawrence0999@gmail.com>
2025-11-15 12:58:21 +0800

    feat: add ensemble.py of comparison

--- scripts/comparison/ensemble_CTRGCN.sh
+++ scripts/comparison/ensemble_CTRGCN.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-GPU_ID="$1"         # 第一個參數：GPU ID 
-JOINT_DIR="$2"
-JOINTV_DIR="$3"
-BONE_DIR="$4"
-BONEV_DIR="$5"
-
+GPU_ID="$1"
+FOLD_NUM="$2"
+JOINT_DIR="$3"
+JOINTV_DIR="$4"
+BONE_DIR="$5"
+BONEV_DIR="$6"
 
 CUDA_VISIBLE_DEVICES="$GPU_ID" python -m comparison.CTRGCN.ensemble \
+    --fold_num "$FOLD_NUM" \
     --joint-dir "$JOINT_DIR" \
     --joint-motion-dir "$JOINTV_DIR" \
     --bone-dir "$BONE_DIR" \
